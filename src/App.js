7774eeda96bd3ae02db500e41eb59112e0dfc5a9
mouni025 <im.mounika025@gmail.com>
2023-02-03 22:37:37 +0530

    Added form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-100vh flex justify-center text-center align-middle">
       <Form/>
       
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,25 @@ import React from "react";
 
 function Form(){
     return(
-        <div className="w-100 font-bold ">
-            Sign In
+        <section>
+        <div className="max-w-978 w-100 flex border-1 border-black ">
+            <div className="col-1">
+                <h2>Sign In</h2>
+                <span>Register and enjoy the service</span>
 
+                <form id='form'  >
+                    <input type="text" placeholder='username' />
+                    <input type="text"  placeholder='password' />
+                    <input type="text" placeholder='confirm password' />
+                    <input type="text" placeholder='mobile number' />
+                    
+                    <button className='btn'>Sign In</button>
+                </form>
+
+            </div>
+            
         </div>
+    </section>
     )
 }
 
